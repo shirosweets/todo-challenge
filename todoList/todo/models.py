d124b46c74376768d4a12ddf8fff36acad8cf429
danilodiez <danilodiez1@hotmail.com>
2021-01-08 16:16:12 -0300

    Creacion de modelo de la tarea

--- todoList/todo/models.py
+++ todoList/todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+    pub_date = models.DateTimeField('fecha de publicacion')
+    content_text = models.CharField(max_length=200)
+    completed = models.BooleanField(completed = False)
+
+    
